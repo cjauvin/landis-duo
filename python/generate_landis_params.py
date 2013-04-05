2b2cd20e1c639d8979c62249632a01c1c6ee1122
Christian Jauvin <cjauvin@gmail.com>
2013-04-05 14:06:38 -0400

    negative bug

--- python/generate_landis_params.py
+++ python/generate_landis_params.py
@@ -5,11 +5,11 @@ from collections import OrderedDict, defaultdict
 R = 3
 M = 25
 
-#loc = 'sims_n=216'
-loc = 'sims_n=4536'
+input_path = '../EC2Package'
+result_path = 'sims_n=4536' # 216
 
-os.system('rm -fr %s' % loc)
-os.mkdir(loc)
+os.system('rm -fr %s' % result_path)
+os.mkdir(result_path)
 
 aos_header = """LandisData  "Age-only Succession"
 
@@ -31,7 +31,7 @@ EstablishProbabilities
 params = OrderedDict() # OD because we need to keep keys in order
 header = [] # top part (i.e. non-table) of file
 
-f = open('../EstablishProbabilities_PGInputs.csv')
+f = open('%s/EstablishProbabilities_PGInputs.csv' % input_path)
 cols = f.readline().split(',')
 
 for line in f:
@@ -40,18 +40,20 @@ for line in f:
     species, ecoregion = values['species'], values['ecoregion']
     params.setdefault(species, OrderedDict())
     params[species][ecoregion] = [float(values[c]) for c in ['min', 'max', 'ccMin', 'ccMax']]
-    assert params[species][ecoregion][0] <= params[species][ecoregion][1]
-    assert params[species][ecoregion][2] <= params[species][ecoregion][3]
+    assert params[species][ecoregion][0] <= params[species][ecoregion][1] and params[species][ecoregion][0] >= 0 and \
+      params[species][ecoregion][0] <= 1 and params[species][ecoregion][1] >= 0 and params[species][ecoregion][1] <= 1
+    assert params[species][ecoregion][2] <= params[species][ecoregion][3] and params[species][ecoregion][2] >= 0 and \
+      params[species][ecoregion][2] <= 1 and params[species][ecoregion][3] >= 0 and params[species][ecoregion][3] <= 1
 
 ecoregions = next(params.itervalues()).keys()
 
 def copy_files(n, bf, bw, sp):
-    os.system('cp ../scenario.txt %s/%s/' % (loc, n))
-    os.system('cp ../ExtractCohorts.r %s/%s/' % (loc, n))
-    os.system('cp ../SimpleBatchFile.bat %s/%s/' % (loc, n))
-    os.system('cp ../base-fire-6.0_%s.txt %s/%s/base-fire.txt' % (bf, loc, n))
-    os.system('cp ../base-wind_%s.txt %s/%s/base-wind.txt' % (bw, loc, n))
-    os.system('cp ../species%s.txt %s/%s/species.txt' % (sp, loc, n))
+    os.system('cp %s/scenario.txt %s/%s/' % (input_path, result_path, n))
+    os.system('cp %s/ExtractCohorts.r %s/%s/' % (input_path, result_path, n))
+    os.system('cp %s/SimpleBatchFile.bat %s/%s/' % (input_path, result_path, n))
+    os.system('cp %s/base-fire-6.0_%s.txt %s/%s/base-fire.txt' % (input_path, bf, result_path, n))
+    os.system('cp %s/base-wind_%s.txt %s/%s/base-wind.txt' % (input_path, bw, result_path, n))
+    os.system('cp %s/species%s.txt %s/%s/species.txt' % (input_path, sp, result_path, n))
 
 n = 0
 
@@ -68,8 +70,10 @@ for bf in ['hff-bs', 'hff-fs', 'lff-fs']:
                 for ecoregion, vals in params[species].items():
                     size1 = (vals[1] - vals[0]) / M
                     samples1 = [str(round(random.uniform(vals[0] + size1 * i, vals[0] + size1 * (i + 1)), 2)) for i in range(M)]
-                    size2 = (vals[2] - vals[3]) / M
+                    size2 = (vals[3] - vals[2]) / M
                     samples2 = [str(round(random.uniform(vals[2] + size2 * i, vals[2] + size2 * (i + 1)), 2)) for i in range(M)]
+                    assert all(float(v) >=0 for v in samples1)
+                    assert all(float(v) >=0 for v in samples2)
                     #print species, ecoregion, vals, samples
                     random.shuffle(samples1)
                     random.shuffle(samples2)
@@ -79,8 +83,8 @@ for bf in ['hff-bs', 'hff-fs', 'lff-fs']:
             for r in range(R):
                 for cc_idx in [0, 1]:
                     for m in range(M):
-                        os.mkdir('%s/%s' % (loc, n))
-                        fout = open('%s/%s/age-only-succession.txt' % (loc, n), 'w')
+                        os.mkdir('%s/%s' % (result_path, n))
+                        fout = open('%s/%s/age-only-succession.txt' % (result_path, n), 'w')
                         fout.write(aos_header)
                         fout.write('\t'.join([''] + ecoregions) + '\r\n')
                         for species in params.keys():
@@ -92,14 +96,14 @@ for bf in ['hff-bs', 'hff-fs', 'lff-fs']:
                         n += 1
 
                 for aos_special in ['maxCC', 'max', 'meanCC', 'mean', 'minCC', 'min']:
-                    os.mkdir('%s/%s' % (loc, n))
-                    os.system('cp ../age-only-succession_%s.txt %s/%s/age-only-succession.txt' % (aos_special, loc, n))
+                    os.mkdir('%s/%s' % (result_path, n))
+                    os.system('cp %s/age-only-succession_%s.txt %s/%s/age-only-succession.txt' % (input_path, aos_special, result_path, n))
                     copy_files(n, bf, bw, sp)
                     cc_idx = 1 if 'CC' in aos_special else 0
                     exp_map.append([n, bf, bw, sp, aos_special, cc_idx, r])
                     n += 1
 
-f = open('%s_infos.csv' % loc, 'w')
+f = open('%s_infos.csv' % result_path, 'w')
 f.write(','.join(['i', 'base-fire', 'base-wind', 'species', 'm', 'cc', 'r']) + '\r\n')
 for row in exp_map:
     f.write(','.join([str(v) for v in row]) + '\r\n')
